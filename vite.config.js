fa4abc044fba2d69f4e0fa94f6d836a13db81624
Erastus Even <103950275+Evwn@users.noreply.github.com>
2025-06-23 16:34:16 +0300

    aligned the database for migration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
         vue({
